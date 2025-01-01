f37709eba99004c7bd1809dee679d97ce9465e50
Mark Wolfe <mark@wolfe.id.au>
2025-01-01 22:07:43 +1100

    fix: missing server configuration for lambda

--- internal/commands/lambda.go
+++ internal/commands/lambda.go
@@ -6,6 +6,8 @@ import (
 	"io"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog"
@@ -17,6 +19,7 @@ import (
 	zlog "github.com/wolfeidau/lambda-go-extras/middleware/zerolog"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
 
+	"github.com/wolfeidau/zipstash/internal/ciauth"
 	"github.com/wolfeidau/zipstash/internal/server"
 	"github.com/wolfeidau/zipstash/pkg/trace"
 )
@@ -47,9 +50,28 @@ func (s *LambdaServerCmd) Run(ctx context.Context, globals *Globals) error {
 		Fields: map[string]any{"version": globals.Version},
 	}))
 
+	oidc, err := ciauth.NewWithConfig(ctx, ciauth.Config{
+		Providers: ciauth.DefaultProviderEndpoints,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create oidc middleware: %w", err)
+	}
+
+	e.Use(oidc)
+
+	awscfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to load config: %w", err)
+	}
+
+	s3ClientFunc := func() *s3.Client {
+		return s3.NewFromConfig(awscfg)
+	}
+
 	err = server.Setup(ctx, e, server.Config{
 		JWKSURL:     s.JWKSURL,
 		CacheBucket: s.CacheBucket,
+		GetS3Client: s3ClientFunc,
 	})
 	if err != nil {
 		return fmt.Errorf("failed to setup server: %w", err)
